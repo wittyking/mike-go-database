c669c68694b4deb77bbc332de24e627385f39581
witty king <witty.king@gmail.com>
2024-08-25 16:43:19 +0700

    fiber connect db

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
@@ -20,9 +21,9 @@ const (
 var db *sql.DB
 
 type Product struct {
-	ID    int
-	Name  string
-	Price int
+	ID    int    `json: "id"`
+	Name  string `json: "name"`
+	Price int    `json: "price"`
 }
 
 func main() {
@@ -49,31 +50,106 @@ func main() {
 
 	app := fiber.New()
 
-	fmt.Println("Connection Database Successful")
-	////create
-	// err = createProduct(&Product{Name: "Go product2", Price: 444})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	////Read
-	// product, err := getProduct(2)
-	// fmt.Println("Get Successful !", product)
-	////update
-	// product, err := updateProduct(2, &Product{Name: "Lopster111", Price: 400})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	////Delete
-	// err = deleteProduct(4)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	app.Get("/product/:id", getProductHandler)
+	app.Get("/product", createProductsHandler)
+	app.Post("/product", createProductHandler)
+	app.Put("/product/:id", updateProductHandler)
+	app.Delete("/product/:id", deleteProductHandler)
+
+	app.Listen(":8040")
+
+}
+
+func getProductHandler(c *fiber.Ctx) error {
+	productId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	product, err := getProduct(productId)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.JSON(product)
+}
+
+func createProductsHandler(c *fiber.Ctx) error {
 	products, err := getProducts()
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.JSON(products)
+}
+
+func createProductHandler(c *fiber.Ctx) error {
+	p := new(Product)
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	err := createProduct(p)
 
 	if err != nil {
-		log.Fatal(err)
+		return c.SendStatus(fiber.StatusBadRequest)
 	}
-	fmt.Println("Get All Products !", products)
 
-	// print("Create Successful!")
+	return c.JSON(p)
 }
+
+func updateProductHandler(c *fiber.Ctx) error {
+	productId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	p := new(Product)
+	if err := c.BodyParser(p); err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	product, err := updateProduct(productId, p)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	return c.JSON(product)
+}
+
+func deleteProductHandler(c *fiber.Ctx) error {
+	productId, err := strconv.Atoi(c.Params("id"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+	err = deleteProduct(productId)
+	if err != nil {
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
+}
+
+// fmt.Println("Connection Database Successful")
+// ////create
+// // err = createProduct(&Product{Name: "Go product2", Price: 444})
+// // if err != nil {
+// // 	log.Fatal(err)
+// // }
+// ////Read
+// // product, err := getProduct(2)
+// // fmt.Println("Get Successful !", product)
+// ////update
+// // product, err := updateProduct(2, &Product{Name: "Lopster111", Price: 400})
+// // if err != nil {
+// // 	log.Fatal(err)
+// // }
+// ////Delete
+// // err = deleteProduct(4)
+// // if err != nil {
+// // 	log.Fatal(err)
+// // }
+// products, err := getProducts()
+
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// fmt.Println("Get All Products !", products)
+
+// // print("Create Successful!")
